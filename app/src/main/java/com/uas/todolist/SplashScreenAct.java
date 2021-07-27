b062e3082028fe4064ca3247da4996f99f8d7c42
aldididi <aldyh45@gmail.com>
2021-07-28 01:38:18 +0700

    PUSH 1, LAYOUTING MAIN, TASK, SPLASH

---
+++ app/src/main/java/com/uas/todolist/SplashScreenAct.java
@@ -0,0 +1,14 @@
+package com.uas.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
