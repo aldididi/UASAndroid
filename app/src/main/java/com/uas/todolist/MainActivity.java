aa005d25ced1bd56586a205b2a7f8baf77912540
aldididi <aldyh45@gmail.com>
2021-07-28 08:02:06 +0700

    adding tasks, todo model and adapter

--- app/src/main/java/com/uas/todolist/MainActivity.java
+++ app/src/main/java/com/uas/todolist/MainActivity.java
@@ -1,14 +1,49 @@
 package com.uas.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.uas.todolist.Adapter.ToDoAdapter;
+import com.uas.todolist.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel>taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+        ToDoModel task = new ToDoModel();
+        task.setTask("Ini test tugas");
+        task.setStatus(0);
+
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
     }
 }
